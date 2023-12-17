e7975c9882326433172dc660cc7c08717af049fe
unknown <kshitijlm10b@gmail.com>
2023-12-17 22:01:00 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { logo } from "../utils/constants";
+import { SearchBar } from "./";
+
+const Navbar = () => (
+  <Stack direction="row" alignItems="center" p={0.5} sx={{ position:  "sticky", background: 'black', top: 0, justifyContent: "space-between" }}>
+    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+      <img src={logo} alt="logo" height={28} />
+    </Link>
+    <SearchBar />
+  </Stack>
+);
+
+export default Navbar;
